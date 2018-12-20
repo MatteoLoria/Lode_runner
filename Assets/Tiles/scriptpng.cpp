514cd30202e1aed648814c3ea41e9bca247d2ffa
MatteoLoria <mloria58@gmail.com>
2018-12-20 19:19:45 +0100

    level 2&3 done

--- Assets/Tiles/scriptpng.cpp
+++ Assets/Tiles/scriptpng.cpp
@@ -8,14 +8,14 @@ int main(int argc, char const *argv[])
     al_init();
     al_init_image_addon();
     ALLEGRO_BITMAP * toSave=NULL;
-    ALLEGRO_BITMAP * toCut = al_load_bitmap("testgurd.png");
+    ALLEGRO_BITMAP * toCut = al_load_bitmap("3hole.png");
     if(!toCut)
         cerr<<"no";
     int k=8;
-    for(int i=0; i<44;i+=22){
-        for(int j=0; j<220; j+=20, k++){
+    for(int i=2; i<88;i+=44){
+        for(int j=0; j<180; j+=20, k++){
             cout<<i<<" "<<j<<endl;
-            toSave = al_create_sub_bitmap(toCut,j,i,20,20);
+            toSave = al_create_sub_bitmap(toCut,j,i,20,40);
             al_save_bitmap((to_string(k)+".png").c_str(),toSave);
         }
     }
